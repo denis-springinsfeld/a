b629652275a7bae4550aff3e1b96cd5e6e400be7
denis springinsfeld <denis.springinsfeld@unilim.fr>
2025-10-22 15:06:21 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/a",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        accueil: resolve(__dirname, "accueil.html"),
+      },
+    },
+  },
 });
