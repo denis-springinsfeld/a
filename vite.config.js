98d969f4a22d09801851a0843827648a67276550
denis springinsfeld <denis.springinsfeld@unilim.fr>
2025-10-22 15:14:28 +0200

    final

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/a",
+  base: "/a/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         accueil: resolve(__dirname, "accueil.html"),
+        plus: resolve(__dirname, "plus.html"),
+        // ajoutez d'autres pages si nécessaire
       },
     },
   },
